94dd930193003df9dc725566e22f2d1479851c31
Yuri Avdijevski <dtoxvanilla@gmail.com>
2021-12-02 18:24:19 +0000

    solved another problem

--- oddEvenLinkedList.js
+++ oddEvenLinkedList.js
@@ -10,5 +10,29 @@
  * @return {ListNode}
  */
 var oddEvenList = function (head) {
+    if (!head) return null;
+    if (!head.next) return head;
+    let result = head;
+    let oddP = head;
+    let evenP = head.next;
+    let connector = head.next;
+    while (oddP && evenP) {
+        // 1 (odd) -> 2 (even) -> NULL
+        if (!evenP.next) break;
+        // 1 (odd) -> 2 (even) -> 3 -> 4 -> 5 -> NULL
+        // 3 = 2->3
+        let temp = evenP.next;
+        // 1->next = 3
+        oddP.next = temp;
+        // 2->next = 3->4
+        evenP.next = temp.next;
+        // 3
+        oddP = temp;
+        // 4
+        evenP = temp.next;
+    }
+    oddP.next = connector;
+    return result;
 
 };
+console.log(oddEvenList([1, 2, 3, 4, 5]));
